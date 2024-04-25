deeb76c0c68d2397b758cc9b751c122e5209385f
Galuh Ramadhan <galuh.html@gmail.com>
2024-04-25 21:04:06 +0700

    fix: preview project

---
+++ src/components/PreviewProject.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Box from './Box'
+import { Cross2Icon } from '@radix-ui/react-icons'
+
+const PreviewProject = ({ currentPreviewData, onClose }) => {
+    return (
+        <Box
+            css={{
+                position: 'fixed',
+                top: 0,
+                background: '#000000ab',
+                left: 0,
+                height: '100%',
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}
+        >
+            <Box
+                css={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    background: '#FFF',
+                    maxWidth: '95%',
+                    width: `${window.innerHeight}px`,
+                    'a': {
+                        border: 0,
+                        padding: 0,
+                    }
+                }}
+            >
+                <Box
+                    css={{
+                        padding: '0 15px',
+                        display: 'grid',
+                        gap: 10,
+                        gridTemplateColumns: '1fr auto',
+                        'button': {
+                            background: 'transparent',
+                            outline: 'none',
+                            boxShadow: 'none',
+                            border: 0,
+                            color: '#000',
+                            cursor: 'pointer'
+                        },
+                    }}>
+                    <h5>Please click on the image below to visit.</h5>
+                    <button type="button" onClick={onClose}><Cross2Icon height={36} width={36} /></button>
+                </Box>
+                <a style={{ background: '#212121', border: 0, padding: 0 }} href={currentPreviewData.url} target="_blank" rel="noreferrer"><img alt="project preview" src={currentPreviewData.image} style={{ width: '100%' }} /></a>
+            </Box>
+        </Box>
+    )
+}
+
+export default PreviewProject
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,100 +1,129 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import Box from '../components/Box'
+import PreviewProject from '../components/PreviewProject'
+
+const previewData = {
+  dashboard: {
+    image: '/document/dashboard.png',
+    url: 'https://dashboard.majoo.id'
+  },
+  cockpit: {
+    image: '/document/cockpit.png',
+    url: 'https://cockpit.mangkujagat.com'
+  },
+  simpeg: {
+    image: '/document/simpeg.png',
+    url: 'https://github.com/pecelpagi/simpeg-client'
+  },
+  laundria: {
+    image: '/document/laundria.png',
+    url: 'https://github.com/pecelpagi/laundria-client'
+  }
+}
 
 const Projects = () => {
+  const [currentPreviewData, setCurrentPreviewData] = useState(null);
+
+  const handlePreview = (key) => {
+    setCurrentPreviewData(previewData[key]);
+  }
+
   return (
-    <Box css={{
-      paddingBottom: 100,
-      '.page-title': {
-        marginBottom: 20,
-      }
-    }}>
-      <Box>
-        <h1 className='page-title'>Projects.</h1>
-        <span>If you would like to see all my projects, please download the documentation in <a href="/document/PORTFOLIO_GALUH.pdf">here</a>.</span>
-      </Box>
-      <Box
-        css={{
-          marginTop: '2em',
-          display: 'grid',
-          gap: 25,
-          gridTemplateColumns: '1fr',
-          '@sm':  {
-            gridTemplateColumns: '1fr 1fr',
-          },
-          '.project': {
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 20,
-            'h1': {
-              padding: 0,
-              margin: 0,
-              fontSize: '1.25em'
+    <Fragment>
+      <Box css={{
+        paddingBottom: 100,
+        '.page-title': {
+          marginBottom: 20,
+        }
+      }}>
+        <Box>
+          <h1 className='page-title'>Projects.</h1>
+          <span>If you would like to see all my projects, please download the documentation in <a href="/document/PORTFOLIO_GALUH.pdf">here</a>.</span>
+        </Box>
+        <Box
+          css={{
+            marginTop: '2em',
+            display: 'grid',
+            gap: 25,
+            gridTemplateColumns: '1fr',
+            '@sm': {
+              gridTemplateColumns: '1fr 1fr',
             },
-            'span': {
-              fontWeight: 'normal',
-              flex: '1 1 0px',
-            },
-            padding: 25,
-            border: '1px solid #d6dbe6',
-            '.preview': {
-              marginTop: 20,
-              border: '2px solid #000',
-              padding: '10px 15px',
-              borderRadius: 0,
-              height: 'fit-content',
-              fontSize: 14,
-              width: 'fit-content'
-            },
-            '&:hover': {
-              border: '1px solid #000',
-              cursor: 'pointer',
+            '.project': {
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 20,
+              'h1': {
+                padding: 0,
+                margin: 0,
+                fontSize: '1.25em'
+              },
+              'span': {
+                fontWeight: 'normal',
+                flex: '1 1 0px',
+              },
+              padding: 25,
+              border: '1px solid #d6dbe6',
               '.preview': {
-                background: '#000',
-                color: '#FFF',
-              }
-            },
-          }
-        }}
-      >
-        <Box className='project'>
-          <h1>Majoo Sales Dashboard Web Application</h1>
-          <span>
-            A web application that business owners use to monitor their sales. It has been used by thousands of business owners in Indonesia.
-          </span>
-          <div className='preview'>
-            Preview
-          </div>
-        </Box>
-        <Box className='project'>
-          <h1>Majoo Internal Dashboard Web Application</h1>
-          <span>
-            A web application to monitor majoo users, manage user submissions, and more.
-          </span>
-          <div className='preview'>
-            Preview
-          </div>
-        </Box>
-        <Box className='project'>
-          <h1>Laundria</h1>
-          <span>
-            Web application for laundry management.
-          </span>
-          <div className='preview'>
-            Preview
-          </div>
-        </Box>
-        <Box className='project'>
-          <h1>SIMPEG</h1>
-          <span>
-            Web application for employee management.
-          </span>
-          <div className='preview'>
-            Preview
-          </div>
+                marginTop: 20,
+                border: '2px solid #000',
+                padding: '10px 15px',
+                borderRadius: 0,
+                height: 'fit-content',
+                fontSize: 14,
+                width: 'fit-content'
+              },
+              '&:hover': {
+                border: '1px solid #000',
+                cursor: 'pointer',
+                '.preview': {
+                  background: '#000',
+                  color: '#FFF',
+                }
+              },
+            }
+          }}
+        >
+          <Box className='project' onClick={() => { handlePreview('dashboard'); }}>
+            <h1>Majoo Sales Dashboard Web Application</h1>
+            <span>
+              A web application that business owners use to monitor their sales. It has been used by thousands of business owners in Indonesia.
+            </span>
+            <div className='preview'>
+              Preview
+            </div>
+          </Box>
+          <Box className='project' onClick={() => { handlePreview('cockpit'); }}>
+            <h1>Majoo Internal Dashboard Web Application</h1>
+            <span>
+              A web application to monitor majoo users, manage user submissions, and more.
+            </span>
+            <div className='preview'>
+              Preview
+            </div>
+          </Box>
+          <Box className='project' onClick={() => { handlePreview('laundria'); }}>
+            <h1>Laundria</h1>
+            <span>
+              Web application for laundry management.
+            </span>
+            <div className='preview'>
+              Preview
+            </div>
+          </Box>
+          <Box className='project' onClick={() => { handlePreview('simpeg'); }}>
+            <h1>SIMPEG</h1>
+            <span>
+              Web application for employee management.
+            </span>
+            <div className='preview'>
+              Preview
+            </div>
+          </Box>
         </Box>
       </Box>
-    </Box>
+      {currentPreviewData && <PreviewProject {...{ currentPreviewData }} onClose={() => { setCurrentPreviewData(null); }} />}
+    </Fragment>
   )
 }
 
