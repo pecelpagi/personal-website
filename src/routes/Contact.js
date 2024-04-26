c5713da88a87ae440c29fe5b877907371c047d0b
Galuh Ramadhan <galuh.html@gmail.com>
2024-04-26 13:49:45 +0700

    fix: contact

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Box from '../components/Box'
-import { Link } from 'react-router-dom'
 
 const Contact = () => {
   return (
     <Box css={{
+      width: '100%',
+      overflowX: 'auto',
       paddingBottom: 100,
       'ul': {
         marginLeft: '1em',
@@ -22,6 +23,7 @@ const Contact = () => {
       <ul>
         <li><Box>Mobile:</Box><Box>+6285 731 762 554</Box></li>
         <li><Box>Email: </Box><a href="mailto:galuhrmdh@gmail.com">galuhrmdh@gmail.com</a></li>
+        <li><Box>Upwork: </Box>< a href="https://www.upwork.com/freelancers/~0194b57790cf1ccd33" target='_blank' rel="noreferrer">https://www.upwork.com/freelancers/~0194b57790cf1ccd33</a></li>
         <li><Box>LinkedIn:</Box><a href="https://www.linkedin.com/in/galuhrmdh" target='_blank' rel="noreferrer">https://www.linkedin.com/in/galuhrmdh</a></li>
         <li><Box>GitHub:</Box><a href="https://github.com/pecelpagi" target='_blank' rel="noreferrer">https://github.com/pecelpagi</a></li>
         <li><Box>Instagram:</Box><a href="https://www.instagram.com/pecelpagi_" target='_blank' rel="noreferrer">https://www.instagram.com/pecelpagi_</a></li>
